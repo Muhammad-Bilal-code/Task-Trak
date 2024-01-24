08aeb1e509dc8f3d0507033c50825b9a0b85e411
Muhammad Bilal <mohammadbillal268@gmail.com>
2024-01-24 12:35:19 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline text-red-500">Test</h2>
+    </>
+  );
+}
+
+export default App;
